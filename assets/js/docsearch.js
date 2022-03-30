829a4d6f9a16e241cc29e2288bf1452ed5489713
totallynotdavid <dadch1404@gmail.com>
2022-03-30 03:51:06 -0500

    Changed search engine and fixed Blog (was not closed)

--- assets/js/docsearch.js
+++ assets/js/docsearch.js
@@ -23,8 +23,8 @@ if (searchPlaceholder !== null) {
 }
 
 algoliasearchNetlify({
-  appId: 'JE37E4YOU3',
-  apiKey: '0b06babcb9d1b0aaadaa9e9af5be7bfe',
+  appId: 'B36UGT1YWJ',
+  apiKey: '8c770925ae0a24146f978f0495f51ec1',
   siteId: 'abc167f5-8dee-48db-a7da-3c97c18849be',
   branch: 'master',
   selector: '#docsearch',
