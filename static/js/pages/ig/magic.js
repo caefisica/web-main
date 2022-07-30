dfe181800c3a7b9181c9cf0338aa23634c870753
David D <dadch1404@gmail.com>
2022-07-30 07:53:30 -0500

    fix: not longer captions please #67

--- static/js/pages/ig/magic.js
+++ static/js/pages/ig/magic.js
@@ -1,5 +1,11 @@
 var feed = new Instafeed({
-    accessToken: InstagramToken
-    , debug: true
+    accessToken: InstagramToken, 
+    debug: false,
+
+    transform: function(item) {
+      item.caption = item.caption.replace(/\s+/g," ").split(/(?=\s)/gi).slice(0, 40).join('');
+      return item;
+    },
+
 });
 feed.run();
