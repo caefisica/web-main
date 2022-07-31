61b38f3127a7552e57d603e88d3babe15fc085ba
David D <dadch1404@gmail.com>
2022-07-31 15:59:33 -0500

    fix: #70 mjs need to be ES modules

--- functions/instagram.mjs
+++ functions/instagram.mjs
@@ -3,7 +3,7 @@ import './loadenv.mjs';
 const { INSTAGRAM_KEY } = process.env
 var IG = `${INSTAGRAM_KEY}`;
 
-exports.handler = async function (event, context) {
+export const handler = async function (event, context) {
   return {
     statusCode: 200,
     body: JSON.stringify(IG),
