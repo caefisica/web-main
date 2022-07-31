0a30388591c64a4c4e58e11d026edca6aaf50c73
David D <dadch1404@gmail.com>
2022-07-31 15:28:29 -0500

    feat: netlify functions & instagram feed

--- static/js/pages/ig/magic_public.js
+++ static/js/pages/ig/magic_public.js
@@ -1,12 +1,21 @@
-var feed = new Instafeed({
-    accessToken: INSTAGRAM_KEY,
-    debug: false,
-    limit: 9,
-    template: '<div class="col-md"> <div class="card"> <div class="card-img"> <a href="{{image}}" data-fancybox="gallery" data-caption="{{caption}}"> <img src="{{image}}" loading="lazy" width="200" height="200" alt="Descripción: {{caption}}"></img> </a> </div></div></div>',
-    transform: function(item) {
-      item.caption = item.caption.replace(/\s+/g," ").split(/(?=\s)/gi).slice(0, 50).join('');
-      return item;
-    },
-});
+fetch('/.netlify/functions/instagram')
+  .then(function(resp) {
+    return resp.json();
+  })
+  .then(function(data) {
+    let insvar = data;
 
-feed.run();
+    var feed = new Instafeed({
+      accessToken: insvar,
+      debug: false,
+      limit: 9,
+      template: '<div class="col-md"> <div class="card"> <div class="card-img"> <a href="{{image}}" data-fancybox="gallery" data-caption="{{caption}}"> <img src="{{image}}" loading="lazy" width="200" height="200" alt="Descripción: {{caption}}"></img> </a> </div></div></div>',
+      transform: function(item) {
+        item.caption = item.caption.replace(/\s+/g," ").split(/(?=\s)/gi).slice(0, 50).join('');
+        return item;
+      },
+    });
+    
+    feed.run();
+
+  })
