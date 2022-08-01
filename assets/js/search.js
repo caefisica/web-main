ce7b8dd1301134ba921a000da03e088bacb154f6
David D <dadch1404@gmail.com>
2022-07-31 21:18:32 -0500

    #63 feat: focus on shortcut and minor ui fixes

---
+++ assets/js/search.js
@@ -0,0 +1,7 @@
+document.onkeyup = function () {
+  var e = e || window.event; // for IE to cover IEs window event-object
+  if(e.ctrlKey && e.key === '/' ) {
+      document.getElementById("autocomplete-0-input").focus();
+    return false;
+  }
+}
