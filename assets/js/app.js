188561d2a7ceeae3f73363b28d110fa604d3568c
David D <dadch1404@gmail.com>
2022-11-29 14:22:27 -0500

    fix: lint & delete comments

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,52 +1,3 @@
-/*
-var docsearch = document.getElementById('docsearch');
-
-if (docsearch !== null) {
-  document.addEventListener('click', buttonBlur);
-}
-
-function buttonBlur() {
-  docsearch.firstElementChild.blur();
-}
-*/
-
-/*
-var search = document.getElementById('search');
-
-if (search !== null) {
-  document.addEventListener('keydown', inputFocus);
-}
-
-function inputFocus(e) {
-  if (e.ctrlKey && e.key === '/' ) {
-    e.preventDefault();
-    search.focus();
-  }
-  if (e.key === 'Escape' ) {
-    search.blur();
-  }
-}
-*/
-
-/* eslint-disable */
-/*
-if (document.querySelector('#search') !== null) {
-  docsearch({
-    apiKey: '978789745e8a1cf3e9a3f0fa321dbdb9',
-    indexName: 'getdoks',
-    inputSelector: '#search',
-    debug: false,
-  });
-}
-*/
-/* eslint-enable */
-
-/*
- * https://css-tricks.com/using-netlify-forms-and-netlify-functions-to-build-an-email-sign-up-widget/
- * https://answers.netlify.com/t/how-to-include-dependencies-in-netlify-lambda-functions/2323/38
-*/
-
-/* eslint-disable */
 const processForm = form => {
   const data = new FormData(form)
   data.append('form-name', 'newsletter');
@@ -55,10 +6,10 @@ const processForm = form => {
     body: data,
   })
   .then(() => {
-    form.innerHTML = '<p class="form--success"><strong>Casi terminamos</strong>, revisa en tu correo en búsqueda del mensaje de confirmación.</p>';
+    form.innerHTML = '<p class="form--success mb-0"><strong>Casi terminamos</strong>, revisa en tu correo en búsqueda del mensaje de confirmación.</p>';
   })
   .catch(error => {
-    form.innerHTML = '<p class="form--error"><strong>Error:</strong> ${error}</p>';
+    form.innerHTML = `<p class="form--error mb-0">Oops. Algo no salió bien: ${error}</p>`;
   })
 }
 
@@ -69,4 +20,3 @@ if (emailForm) {
     processForm(emailForm);
   })
 }
-/* eslint-enable */
