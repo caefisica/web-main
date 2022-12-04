e985a49a0a0907686e7d0d572e951b2a3e10971b
David D <dadch1404@gmail.com>
2022-12-03 21:48:36 -0500

    fix: inline script from /admin

---
+++ static/admin/script.js
@@ -0,0 +1,11 @@
+CMS.registerPreviewStyle("/cms_style.css");
+
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on("init", user => {
+    if (!user) {
+      window.netlifyIdentity.on("login", () => {
+        document.location.href = "/admin/";
+      });
+    }
+  });
+}
