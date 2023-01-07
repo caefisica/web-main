2609060b4f7e7d4a2b3ec2e3a20b2db080443626
totallynotdavid <dadch1404@gmail.com>
2023-01-07 11:46:46 -0500

    feat: added Feature section

--- layouts/_default/index.js
+++ layouts/_default/index.js
@@ -1,10 +1,10 @@
-var pregrado = [
-{{ range $index, $page := (where .Site.Pages "Section" "guias") -}}
-  {
-    id: {{ $index }},
-    title: "{{ .Title }}",
-    description: "{{ .Params.description }}",
-    href: "{{ .URL | relURL }}"
-  },
-{{ end -}}
-];
+var unmsm = [
+  {{ range $index, $page := (where .Site.Pages "Section" "unmsm") -}}
+    {
+      id: {{ $index }},
+      title: "{{ .Title }}",
+      description: "{{ .Params.description }}",
+      href: "{{ .URL | relURL }}"
+    },
+  {{ end -}}
+  ];
