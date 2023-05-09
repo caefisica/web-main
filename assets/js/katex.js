203ad526a02891a59493e4582627f4965d8cc8dc
David Duran <dadch1404@gmail.com>
2023-05-08 23:02:48 -0500

    feat: :sparkles: use references in katex

--- assets/js/katex.js
+++ assets/js/katex.js
@@ -4,7 +4,13 @@ document.addEventListener('DOMContentLoaded', function() {
       {left: '$$', right: '$$', display: true},
       {left: '$', right: '$', display: false},
       {left: '\\(', right: '\\)', display: false},
-      {left: '\\[', right: '\\]', display: true},
+      {left: '\\[', right: '\\]', display: true}, 
     ],
+    trust: (context) => ['\\htmlId', '\\href'].includes(context.command),
+    macros: {
+      "\\eqref": "\\href{###1}{(\\text{#1})}",
+      "\\ref": "\\href{###1}{\\text{#1}}",
+      "\\label": "\\htmlId{#1}{}"
+    },
   });
 });
