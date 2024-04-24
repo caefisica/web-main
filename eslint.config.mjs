23cb2de470a06df58306bb96dbc86b975d3c3bc4
David Duran <dadch1404@gmail.com>
2024-04-23 21:47:02 -0500

    pkgs: update eslint config (enable backticks) & install prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,43 +1,50 @@
-import pluginJs from "@eslint/js";
-import globals from "globals";
+import pluginJs from '@eslint/js';
+import globals from 'globals';
 
 export default [
-  {
-    files: ["**/*.js"],
-    ignores: [
-      "assets/js/index.js",
-      "assets/js/katex.js",
-      "assets/js/vendor",
-      "node_modules",
-    ],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.commonjs,
-        ...globals.es6,
-        ...globals.node,
-        Atomics: "readonly",
-        SharedArrayBuffer: "readonly",
-      },
-      ecmaVersion: 2018,
-      sourceType: "module",
-    },
-  },
-  {
-    ...pluginJs.configs.recommended,
-    rules: {
-      "no-console": 0,
-      "quotes": ["error", "single"],
-      "comma-dangle": [
-        "error",
-        {
-          "arrays": "always-multiline",
-          "objects": "always-multiline",
-          "imports": "always-multiline",
-          "exports": "always-multiline",
-          "functions": "ignore",
+    {
+        files: ['**/*.js'],
+        ignores: [
+            'assets/js/index.js',
+            'assets/js/katex.js',
+            'assets/js/vendor',
+            'node_modules',
+        ],
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.commonjs,
+                ...globals.es6,
+                ...globals.node,
+                Atomics: 'readonly',
+                SharedArrayBuffer: 'readonly',
+            },
+            ecmaVersion: 2018,
+            sourceType: 'module',
         },
-      ],
     },
-  },
+    {
+        ...pluginJs.configs.recommended,
+        rules: {
+            'no-console': 0,
+            'quotes': [
+                'error',
+                'backtick',
+                { avoidEscape: true, allowTemplateLiterals: false },
+            ],
+            'semi': [`error`, `always`],
+            'multiline-ternary': [`error`, `always-multiline`],
+            'no-multi-spaces': [`error`],
+            'comma-dangle': [
+                'error',
+                {
+                    arrays: 'always-multiline',
+                    objects: 'always-multiline',
+                    imports: 'always-multiline',
+                    exports: 'always-multiline',
+                    functions: 'ignore',
+                },
+            ],
+        },
+    },
 ];
